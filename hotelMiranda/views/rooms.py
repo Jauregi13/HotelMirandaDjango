b212f735715d726a12aa77b2b9349754bee5fa57
jauregi13 <jjauregibar13@gmail.com>
2024-02-08 21:16:22 +0100

    plantilla offer terminada

--- hotelMiranda/views/rooms.py
+++ hotelMiranda/views/rooms.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
+from django.db.models import F,ExpressionWrapper, fields
 from ..models import Room
 
 
 def getRooms(request):
+
     rooms = Room.objects.all()
     print(rooms)
     title = 'Rooms'
     title_page = 'Ultimate Room'
-    return render(request, 'hotelMiranda/rooms.html',{"rooms": rooms, "title": title, "title_page": title_page, "breadcrumb": title})
+    return render(request, 'hotelMiranda/rooms.html',
+    {"rooms": rooms, "title": title, "title_page": title_page, "breadcrumb": title})
 
 def getRoomById(request, room_id):
 
@@ -21,8 +24,17 @@ def getRoomById(request, room_id):
         raise Http404('Room does not exist')
 
 def getRoomsOffer(request):
-    rooms = Room.objects.all()
-    return render(request, 'hotelMiranda/offers.html',{"rooms": rooms})
+
+    rooms = Room.objects.annotate(
+        final_price = ExpressionWrapper(
+            F('price') - F('price')*(F('offer') / 100),
+            output_field=fields.IntegerField()
+        )
+    ).filter(offer__gt = 0).values()
+    title = 'Offers'
+    title_page = 'Our Offers'
+    return render(request, 'hotelMiranda/offers.html',
+    {"rooms": rooms, 'title': title, 'title_page': title_page, 'breadcrumb': title})
 
 def getRoomsAvailable(request):
     pass
