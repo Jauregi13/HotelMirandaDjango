062856dac5d9cafacebccb54ffad578cd593307c
jauregi13 <jjauregibar13@gmail.com>
2024-02-23 01:59:53 +0100

    cambiando ubicación de los ficheros estaticos en el servidor

--- hotelMirandaProj/settings.py
+++ hotelMirandaProj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.39.83.64']
+ALLOWED_HOSTS = ['127.0.0.1','13.39.83.64']
 
 
 # Application definition
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "hotelMiranda/static"
 ]
 
-STATIC_ROOT = '/var/www/hotelmiranda-static/'
+STATIC_ROOT = '/var/www/hotelmiranda-static/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
