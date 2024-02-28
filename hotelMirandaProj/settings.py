2f018ef460e6848b4e2408470aaec7ac1807afaa
jauregi13 <jjauregibar13@gmail.com>
2024-02-28 19:10:12 +0100

    server name modificado

--- hotelMirandaProj/settings.py
+++ hotelMirandaProj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','13.39.83.64']
+ALLOWED_HOSTS = ['127.0.0.1','ec2-13-39-83-64.eu-west-3.compute.amazonaws.com']
 
 
 # Application definition
